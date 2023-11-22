00a3300b41ab58d2dd0b77c3b727b53c9f1ab4d8
Sameeksha Kadam <sameekshakadam23@gmail.com>
2023-11-22 15:04:06 +0530

    validation

---
+++ Main/LoginRegister/Login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Login</title>
+    
+</head>
+<body>
+<section class="h-screen">
+  <div class="container h-full px-6 py-24">
+    <div
+      class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+      <!-- Left column container with background-->
+      <div class="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+        <img src="bg.jpg" alt="Phone image" />
+      </div>
+
+      <!-- Right column container with form -->
+      <div class="md:w-8/12 lg:ml-6 lg:w-5/12">
+      <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center mb-3">
+                  Login to your account
+              </h1>
+        <form id="loginForm" class="space-y-4 md:space-y-6 mb-10">
+          
+                <!-- Email input -->
+                <div class="w-full">
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                      <span id="emailError" class="text-red-500"></span>
+                </div>          
+              
+                <!-- Password input -->
+                <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <span id="passwordError" class="text-red-500"></span>
+                </div>
+
+
+          <!-- Submit button -->
+          <button type="submit" class="w-full border text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" style="background-color: black;">Login Now</button>
+          <div class="login_signup">Don't have an account? <a href="register.php" id="signup" style="color: blue;">Signup</a></div>
+          
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script>
+  $(document).ready(function(){
+    $("#loginForm").submit(function(e){
+      var email = $("#email").val();
+      var password = $("#password").val();
+      var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+      // Reset errors
+      $("#emailError").text("");
+      $("#passwordError").text("");
+
+      // Validate email
+      if (!emailRegex.test(email)) {
+        $("#emailError").text("Invalid email address");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate password (you can add more conditions)
+      if (password.length < 8) {
+        $("#passwordError").text("Password must be at least 8 characters");
+        e.preventDefault(); // Prevent form submission
+      }
+    });
+  });
+</script>
+
+</body>
+</html>
--- Main/LoginRegister/register.php
+++ Main/LoginRegister/register.php
@@ -3,186 +3,158 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Register</title>    
 </head>
 <body>
-<!-- <form>
-
-<section class="h-screen">
-  <div class="container h-full px-6 py-24">
-    <div
-      class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-      Left column container with background
-      <div class="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
-        <img
-          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-          class="w-full"
-          alt="Phone image" />
-      </div>
- Right column container with form -->
-      <!-- <div class="md:w-8/12 lg:ml-6 lg:w-5/12">
-      <div class="space-y-12">
-    <div class="border-b border-gray-900/10 pb-12">
-      <h2 class=" font-semibold leading-7 text-gray-900 text-center text-2xl">Register</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600 text-center">Enter your Personal Information</p>
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="col-span-full">
-          <label for="full-name" class="block text-sm font-medium leading-6 text-gray-900">Full name</label>
-          <div class="mt-2">
-            <input type="text" name="full-name" id="full-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-         <div class="sm:col-span-3">
-          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-          <div class="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div> 
-
-        <div class="sm:col-span-full">
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-          <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-
-        <div class="sm:col-span-full">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password (4 to 8 characters max)</label>
-          <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-
-        <div class="sm:col-span-full">
-          <label for="confirm-password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
-          <div class="mt-2">
-            <input type="password" name="confirm-password" id="confirm-password" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-       <div class="sm:col-span-5 sm:col-start-1">
-          <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
-          <div class="mt-2">
-            <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div> 
-
-        <div class="sm:col-span-3">
-          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Flat</label>
-          <div class="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-        <div class="sm:col-span-9">
-          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Bluiding name</label>
-          <div class="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-
-        <div class="sm:col-span-6">
-          <label for="region" class="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
-          <div class="mt-2">
-            <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div> 
-        <div class="col-span-12">
-          <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street address</label>
-          <div class="mt-2">
-            <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-
-        
-
-        <div class="sm:col-span-2">
-          <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
-          <div class="mt-2">
-            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-  </div>
-      </div>
-    </div>
-  </div>
-</section> 
-</form> -->
-
 <section class="h-screen">
   <div class="container h-full px-6 py-24">
     <div
       class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
       <!-- Left column container with background-->
       <div class="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
-        <img
-          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-          class="w-full"
-          alt="Phone image" />
+        <img src="bg.jpg" alt="Phone image" />
       </div>
 
       <!-- Right column container with form -->
       <div class="md:w-8/12 lg:ml-6 lg:w-5/12">
       <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center mb-3">
-                  Create and account
+                  Create an account
               </h1>
-        <form class="space-y-4 md:space-y-6 mb-10">
-          <!-- Email input -->
+        <form id="registerForm" class="space-y-4 md:space-y-6 mb-10">
+          
           <div class="w-full">
                       <label for="full-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
                       <input type="text" name="full-name" id="full-name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Abc Mnq Xyz" required="">
+                      <span id="fullNameError" class="text-red-500"></span>
                 </div>
+                <!-- Email input -->
                 <div class="w-full">
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
                       <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                      <span id="emailError" class="text-red-500"></span>
                 </div>          
                 <div class="w-full">
                       <label for="contact" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Contact</label>
-                      <input type="tel" name="contact" id="contact" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="985******6" required="">
+                      <input type="tel" name="contact" id="contact" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="985****6" required="">
+                      <span id="contactError" class="text-red-500"></span>
                 </div>
-                
+                <!-- Password input -->
                 <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label> 
+                      <div class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"style="color: green;">"Password must be at least 8 characters"</a></div>
                       <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <span id="passwordError" class="text-red-500"></span>
                   </div>
                   <div>
                       <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <span id="confirmPasswordError" class="text-red-500"></span>
                   </div>
-          <!-- Password input -->
-
 
           <!-- flat and building -->
             <div class="flex">
             <div class="w-full mr-1">
                       <label for="flat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Flat</label>
                       <input type="text" name="flat" id="flat" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="A-12" required="">
+                      <span id="flatError" class="text-red-500"></span>
                 </div>
                 <div class="w-full ml-1">
-                      <label for="building" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bluiding</label>
+                      <label for="building" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Building</label>
                       <input type="text" name="building" id="building" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Abc society" required="">
+                      <span id="buildingError" class="text-red-500"></span>
                 </div>
             </div>
                 <div>
-                      <label for="street address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Street Address</label>
-                      <input type="text" name="street address" id="street address" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ndfsf Road, Mulund-E" required="">
+                      <label for="street-address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Street Address</label>
+                      <input type="text" name="street-address" id="street-address" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ndfsf Road, Mulund-E" required="">
+                      <span id="streetAddressError" class="text-red-500"></span>
                 </div>
                
           <!-- Submit button -->
-          <button type="submit" class="w-full border text-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
-
-
+          <button type="submit" class="w-full border text-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" style="background-color: black;">Signup Now</button>
+          <div class="login_signup">Already have an account? <a href="Login.php" id="login" style="color: blue;">Login</a></div>
           
         </form>
       </div>
     </div>
   </div>
 </section>
+
+<script>
+  $(document).ready(function(){
+    $("#registerForm").submit(function(e){
+      var fullName = $("#full-name").val();
+      var email = $("#email").val();
+      var contact = $("#contact").val();
+      var password = $("#password").val();
+      var confirmPassword = $("#confirm-password").val();
+      var flat = $("#flat").val();
+      var building = $("#building").val();
+      var streetAddress = $("#street-address").val();
+
+      // Reset errors
+      $("#fullNameError").text("");
+      $("#emailError").text("");
+      $("#contactError").text("");
+      $("#passwordError").text("");
+      $("#confirmPasswordError").text("");
+      $("#flatError").text("");
+      $("#buildingError").text("");
+      $("#streetAddressError").text("");
+
+      // Validate Full Name
+      if (!fullName.trim()) {
+        $("#fullNameError").text("Full Name is required");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Email
+      var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      if (!emailRegex.test(email)) {
+        $("#emailError").text("Invalid email address");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Contact
+      var contactRegex = /^[0-9]{10}$/;
+      if (!contactRegex.test(contact)) {
+        $("#contactError").text("Invalid contact number");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Password (you can add more conditions)
+      if (password.length < 8) {
+        $("#passwordError").text("Password must be at least 8 characters");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Confirm Password
+      if (password !== confirmPassword) {
+        $("#confirmPasswordError").text("Passwords do not match");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Flat
+      if (!flat.trim()) {
+        $("#flatError").text("Flat is required");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Building
+      if (!building.trim()) {
+        $("#buildingError").text("Building is required");
+        e.preventDefault(); // Prevent form submission
+      }
+
+      // Validate Street Address
+      if (!streetAddress.trim()) {
+        $("#streetAddressError").text("Street Address is required");
+        e.preventDefault(); // Prevent form submission
+      }
+    });
+  });
+</script>
 </body>
 </html>
